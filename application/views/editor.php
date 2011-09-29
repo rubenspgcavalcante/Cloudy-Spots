96862f9a852de84a4fbd062b62d800f021a237d1
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2011-09-28 22:26:14 -0300

    Aplied stylized dropdowns

--- application/views/editor.php
+++ application/views/editor.php
@@ -1,3 +1,35 @@
-<h1>Cloudy Spots</h1>
-<h2>Hello!</h2>
-<a href="<?php echo base_url(); ?>login/exitapp" >Logout</a>
+<div class="content">
+    <div class=" greenbox tiny-bar" id="main_menu">
+        <div id="main-lblock">
+            <ul class="dropdown">
+                <li>
+                    <a href="#">File</a>
+                    <ul class="greenbox sub_menu" >
+                        <li>New</li>
+                        <li>Save</li>
+                        <li>Load</li>
+                    </ul>    
+                </li>
+                <li>
+                    <a href="#">Edit</a>
+                    <ul class="greenbox sub_menu" >
+                        <li>Cut</li>
+                        <li>Copy</li>
+                        <li>Paste</li>
+                    </ul>    
+                </li>
+                <li>
+                    <a href="#">Help</a>
+                    <ul class="greenbox sub_menu" >
+                        <li>About</li>
+                    </ul>
+                </li>
+            </ul>    
+        </div>
+        <div id="main-rblock">
+            <span>Logged as: <?php echo $this->session->userdata("user"); ?></span>
+            <a href="#"><img alt="config" src="<?php echo base_url()?>images/icons/77.png"></a>
+            <a href="<?php echo base_url(); ?>login/exitapp" >logout</a>
+        </div>
+    </div>
+</div>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,7 @@
 		<!-- Javascript load -->
 		<script type="text/javascript" src="<?php echo base_url(); ?>/js/jquery.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>/js/nivo-slider/jquery.nivo.slider.pack.js"></script>
+		<script type="text/javascript" src="<?php echo base_url(); ?>/js/dropdown.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>/js/functions.js"></script>
 	</body>
 </html>
